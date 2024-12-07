3b4f01ac68730336c796a99a977fb79aeb34cf3b
Brady Edwards <bwe731@gmail.com>
2024-12-07 16:47:48 -0600

    Add local files with boilerplate code

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+from constants import *
+
+def main():
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
